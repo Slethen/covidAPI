9c99a5070e619f65af59280155dc17871d131af0
Joel Duncan <joel@slethen.io>
2020-04-18 16:56:25 +0100

    Add sample config

---
+++ sample-config.js
@@ -0,0 +1,10 @@
+// This file must be renamed config.js
+
+var config = {};
+
+// Port API runs on
+config.port = '36625';
+// Frequency data is scraped
+scraperFreq = 100000;
+
+module.exports = config;
